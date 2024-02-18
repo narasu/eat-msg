dcdd602dd49b2fbb0a3f6bf66fa0fad71004c997
narasu <roninnarasu@gmail.com>
2024-02-18 15:27:49 +0100

    fully working game loop

---
+++ Assets/Ji-Ji/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private Action<OrderFailedEvent> orderFailedEventHandler;
+
+
+    private void Awake()
+    {
+        orderFailedEventHandler = OnLose;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.Subscribe(typeof(OrderFailedEvent), orderFailedEventHandler);
+    }
+    
+    private void OnDisable()
+    {
+        EventManager.Unsubscribe(typeof(OrderFailedEvent), orderFailedEventHandler);
+    }
+
+    private void OnLose(OrderFailedEvent _event)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(2);
+    }
+
+}
